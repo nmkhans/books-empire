594f71361854580f4b8c71f0cdefe220d9be3110
Moin Khan <nurmoin05@gmail.com>
2022-04-03 23:16:43 +0600

    router setup done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+		<Header />
+		<Routes>
+			
+		</Routes>
+		{/* Footer */}
     </div>
   );
 }
